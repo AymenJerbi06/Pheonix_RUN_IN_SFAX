080e774ea4778cd681baf51f266934643b5750e1
Aymen Jerbi <aja176@sfu.ca>
2026-04-01 04:08:16 -0700

    changed logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { ArrowRight, Menu, X } from "lucide-react";
-import PhoenixMark from "@/components/PhoenixMark";
 import { navLinks, registrationUrl } from "@/components/siteConfig";
 
 export default function Header() {
@@ -56,7 +56,7 @@ export default function Header() {
               aria-label="Retour en haut de page"
             >
               <div className="flex items-center gap-4">
-                <PhoenixMark compact />
+                <Image src="/images/logo_transparent.png" alt="Phoenix Running Challenge" width={120} height={40} className="h-10 w-auto object-contain" />
                 <div className="hidden border-l border-white/10 pl-4 md:block">
                   <p className="site-display text-[0.72rem] leading-none text-[var(--color-orange)]">
                     Edition 2026
@@ -119,7 +119,7 @@ export default function Header() {
 
         <div className="relative flex h-full flex-col justify-between px-6 pb-8 pt-5">
           <div className="flex items-center justify-between">
-            <PhoenixMark compact />
+            <Image src="/images/logo_transparent.png" alt="Phoenix Running Challenge" width={120} height={40} className="h-10 w-auto object-contain" />
             <button
               type="button"
               onClick={() => setMobileOpen(false)}
